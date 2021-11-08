e482bfb52c6ea6e0ba5bfd35326f3a0b6e346939
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-08 19:11:05 -0300

    comments

--- javaStudies/src/fundamentos/firstProgram.java
+++ javaStudies/src/fundamentos/firstProgram.java
@@ -1,8 +1,21 @@
 package fundamentos;
 
+/*
+ * 
+ *@author wendsant 
+ */
+
 public class firstProgram {
 
 	public static void main(String[] args) {
+		
+		//Comentários
+		/*
+		 * comentario de varias linhas
+		 * 
+		 *linha1
+		 *linha2
+		 * */
 		System.out.println("Hello world");
 	}
 
