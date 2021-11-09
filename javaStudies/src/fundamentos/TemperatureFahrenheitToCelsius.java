c31a7f01b07e72ee04559c1cc2791d76bafcf4db
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-09 18:50:51 -0300

    Update fahrenheitToCelsius

--- javaStudies/src/fundamentos/TemperatureFahrenheitToCelsius.java
+++ javaStudies/src/fundamentos/TemperatureFahrenheitToCelsius.java
@@ -3,15 +3,14 @@ package fundamentos;
 
 public class TemperatureFahrenheitToCelsius {
 		public static void main(String[] args) {
-			double divider, adjust, fahren, celsius;
-			divider = 5.0/9;
-			adjust = -32;
+			double fahren, celsius;
+			final double divider = 5.0/9;
+			final double adjust = -32;
 			fahren = 50;
 			celsius = ((fahren + adjust) * divider);
 			System.out.println("\n ================== \n");
 			System.out.println("FAHRENHEIT PARA CELSIUS");
 			System.out.println("\n ================== \n");
-			System.out.println(celsius);
+			System.out.println("Os graus em celsius é: "+celsius+"ºC.");
 		}
-
 }
