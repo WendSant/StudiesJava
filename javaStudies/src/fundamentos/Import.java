bc7f3a3191c32c77382de9b0d31e1100844f9c4a
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-12 22:12:48 -0300

    import

---
+++ javaStudies/src/fundamentos/Import.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+	public static void main(String[] args) {
+		String g = "Good morning";
+		System.out.println(g);
+		
+		Date d = new Date();
+		System.out.println(d);
+	}
+}
