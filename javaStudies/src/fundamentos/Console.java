c1bcbdfa95613cbffac7785d4950f17071f3ae36
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-13 22:03:11 -0300

    console

---
+++ javaStudies/src/fundamentos/Console.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        System.out.print("Good");
+        System.out.print(" morning!\n\n");
+
+        System.out.println("Good");
+        System.out.println("morning: ");
+
+        System.out.printf("Megasena: %d %d %d %d %d %d", 1, 2, 3, 4, 5, 6);
+        System.out.printf("Salary: %.1f%n", 1234.5678);
+
+        System.out.printf("Name: %s\n", "João");
+
+        Scanner entry = new Scanner(System.in);
+
+        System.out.println("Type your firstname: ");
+        String fName = entry.nextLine();
+        System.out.println("Type your lastname: ");
+        String lName = entry.nextLine();
+        System.out.println("Type your age: ");
+        int age = entry.nextInt();
+
+        System.out.printf("Name: %s %s has %d years old", fName, lName, age);
+
+    }
+}
