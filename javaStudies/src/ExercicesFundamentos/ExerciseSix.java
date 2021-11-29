d434ba3e31b48371756cac99bdfa3163044edda4
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-28 23:07:11 -0300

    Exercise six

---
+++ javaStudies/src/ExercicesFundamentos/ExerciseSix.java
@@ -0,0 +1,21 @@
+package ExercicesFundamentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerciseSix {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+        Scanner entry = new Scanner(System.in);
+        System.out.println("This program calculates the delta of a quadratic equation");
+        System.out.println("Type a number");
+        double a = entry.nextDouble();
+        System.out.println("Type a number");
+        double b = entry.nextDouble();
+        System.out.println("Type a number");
+        double c = entry.nextDouble();
+        double delta = (Math.pow(b, 2) - 4 * a * c);
+        System.out.println(delta);
+        entry.close();
+    }
+}
