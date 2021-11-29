1f7b147f6379e6d2e2c369962dd8e1de3ba74e89
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-29 02:15:58 -0300

    IfElse

---
+++ javaStudies/src/control/IfElse.java
@@ -0,0 +1,17 @@
+package control;
+
+import javax.swing.JOptionPane;
+
+public class IfElse {
+    public static void main(String[] args) {
+        String value = JOptionPane.showInputDialog("Type a number");
+        int number = Integer.parseInt(value);
+
+        if (number % 2 == 0) {
+            System.out.println("even number");
+        } else {
+            System.out.println("odd number");
+        }
+
+    }
+}
