2c3495c08313bc4ee34a9c24ad6e1f5f1d2ee475
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-28 22:01:19 -0300

    Exercise 1

---
+++ javaStudies/src/ExercicesFundamentos/Exercise.java
@@ -0,0 +1,21 @@
+package ExercicesFundamentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+
+        Scanner entry = new Scanner(System.in);
+
+        System.out.println("This program convert fahrenheit to celsius!");
+        System.out.println("Type number: ");
+        double n1 = entry.nextDouble();
+
+        double resultFinal = (n1 - 32) * 5 / 9;
+        System.out.printf("The degree in celsius is %.2f°C%n", resultFinal);
+
+        entry.close();
+    }
+}
