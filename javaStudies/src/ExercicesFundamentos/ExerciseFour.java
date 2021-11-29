d28b0141bc67af60cf899f95cee8cf1425b8cf7e
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-28 22:41:39 -0300

    Exercise four

---
+++ javaStudies/src/ExercicesFundamentos/ExerciseFour.java
@@ -0,0 +1,21 @@
+package ExercicesFundamentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerciseFour {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+        Scanner entry = new Scanner(System.in);
+        System.out.println("Type a number");
+        double n1 = entry.nextDouble();
+
+        double squared = Math.pow(n1, 2);
+        double cubed = Math.pow(n1, 3);
+
+        System.out.printf("The squared number is %.2f and cubed is %.2f%n", squared, cubed);
+
+        entry.close();
+
+    }
+}
