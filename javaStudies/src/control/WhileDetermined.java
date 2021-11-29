8c8bf0e08db4c6291b2779a1ac4d3d36af1fa764
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-29 15:50:35 -0300

    While determined

---
+++ javaStudies/src/control/WhileDetermined.java
@@ -0,0 +1,12 @@
+package control;
+
+public class WhileDetermined {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <= 20) {
+            System.out.printf("i = %d%n", counter);
+            counter += 2;
+
+        }
+    }
+}
