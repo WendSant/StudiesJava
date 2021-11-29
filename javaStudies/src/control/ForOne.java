493249f58baae934b67260686ccd9078125bbc37
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-29 16:08:17 -0300

    for pt1

---
+++ javaStudies/src/control/ForOne.java
@@ -0,0 +1,9 @@
+package control;
+
+public class ForOne {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.printf("i = %d%n", counter);
+        }
+    }
+}
