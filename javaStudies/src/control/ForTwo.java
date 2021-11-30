9c94f3cdb8cf7be13187f872397e5ce6e0482764
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-29 22:13:42 -0300

    for pt2

---
+++ javaStudies/src/control/ForTwo.java
@@ -0,0 +1,9 @@
+package control;
+
+public class ForTwo {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.printf("Counter: %d%n", i);
+        }
+    }
+}
