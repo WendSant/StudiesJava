f20fe812c32b02a1569a6b9673d67bd4a59c0526
Wendson Santana <wendsonsantanajps@gmail.com>
2021-11-30 23:40:44 -0300

    Challenge for

---
+++ javaStudies/src/control/ChallengeFor.java
@@ -0,0 +1,12 @@
+package control;
+
+public class ChallengeFor {
+    public static void main(String[] args) {
+
+        for (String valor = "#"; !valor.equals("#####"); valor += "#") {
+            System.out.println(valor);
+
+        }
+
+    }
+}
