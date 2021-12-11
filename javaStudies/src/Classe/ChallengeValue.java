9ef88953d68a0efc2e0542622540e01dda5eaa69
Wendson <wendsonsantanajps@gmail.com>
2021-12-10 22:52:27 -0300

    challenge value

---
+++ javaStudies/src/Classe/ChallengeValue.java
@@ -0,0 +1,15 @@
+package Classe;
+
+public class ChallengeValue {
+
+    int a = 3;
+    static int b = 7;
+
+    public static void main(String[] args) {
+
+        ChallengeValue m = new ChallengeValue();
+
+        System.out.println(m.a);
+        System.out.println(b);
+    }
+}
