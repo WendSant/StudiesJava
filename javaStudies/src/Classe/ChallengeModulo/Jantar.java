7863a58500d91af33b7d8d9295a34e116966d4e4
Wendson <wendsonsantanajps@gmail.com>
2021-12-14 21:41:04 -0300

    Challenge module

--- javaStudies/src/Classe/ChallengeModulo/Jantar.java
+++ javaStudies/src/Classe/ChallengeModulo/Jantar.java
@@ -1,24 +1,33 @@
 package Classe.ChallengeModulo;
 
+import java.util.Scanner;
+
 public class Jantar {
     public static void main(String[] args) {
-
+        Scanner entry = new Scanner(System.in);
         Pessoa p1 = new Pessoa();
-        p1.nome = "Teste";
-        p1.peso = 100;
-        System.out.println(p1.nome);
+        System.out.println("Enter a name: ");
+        p1.nome = entry.nextLine();
+        System.out.println("Enter a weight: ");
+        p1.peso = entry.nextDouble();
 
         Comida c1 = new Comida();
-        c1.nome = "Rosa";
-        c1.peso = 0.500;
+        System.out.println("Enter the name of the first meal");
+        c1.nome = entry.next();
+        System.err.println("Enter the weight of the first meal");
+        c1.peso = entry.nextDouble();
 
         Comida c2 = new Comida();
-        c2.nome = "Rosa";
-        c2.peso = 0.500;
-
-        p1.comer(c1.peso);
-        p1.comer(c2.peso);
-        System.out.println(p1.peso);
+        System.out.println("Enter the name of the first meal");
+        c2.nome = entry.next();
+        System.out.println("Enter the weight of the first meal");
+        c2.peso = entry.nextDouble();
+        double pesoT = c1.peso + c2.peso;
+        double pesoTPessoa = p1.peso + pesoT;
+        System.out.printf("As refeições foram %s de %.2f gramas e %s de %.2f gramas, dando um peso total de %.2f%n",
+                c1.nome, c1.peso, c2.nome, c2.peso, pesoT);
+        System.out.printf("O peso atual do cliente %s é %.2f%n", p1.nome, pesoTPessoa);
+        entry.close();
 
     }
 }
