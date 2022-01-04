17f802d1dae2410fe8bfc5d042d6b38763c28d5a
Wendson <wendsonsantanajps@gmail.com>
2022-01-03 22:38:42 -0300

    Stack java

---
+++ javaStudies/src/Collections/Stackk.java
@@ -0,0 +1,26 @@
+package Collections;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Stackk {
+    public static void main(String[] args) {
+
+        Deque<String> books = new ArrayDeque<String>();
+
+        books.add("Harry Potter e a pedra de crack");
+        books.push("Percy Jackson e o ladrão de cu");
+        books.push("John Wick");
+
+        System.out.println(books.peek());
+        System.out.println(books.element());
+
+        System.out.println(books.pop());
+        System.out.println(books.poll());
+        System.out.println(books.poll());
+        System.out.println(books.poll());
+        System.out.println(books.poll());
+        System.out.println(books.pop());
+
+    }
+}
