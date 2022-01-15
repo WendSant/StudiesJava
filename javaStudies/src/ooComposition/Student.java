34ebea52d4e2a307da903b3bb490b65efbc17209
Wendson <wendsonsantanajps@gmail.com>
2022-01-14 23:37:39 -0300

    addCourse mano to many

--- javaStudies/src/ooComposition/Student.java
+++ javaStudies/src/ooComposition/Student.java
@@ -8,4 +8,8 @@ public class Student {
     String name;
     List<Course> courses = new ArrayList<>();
 
+    void addCourse(Course course) {
+        this.courses.add(course);
+        course.students.add(this);
+    }
 }
