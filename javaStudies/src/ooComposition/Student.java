4d29dd5c7ac7d8dc7175bc91b43f8a2050e2895f
Wendson <wendsonsantanajps@gmail.com>
2022-01-14 23:55:19 -0300

    Methods add many to many

--- javaStudies/src/ooComposition/Student.java
+++ javaStudies/src/ooComposition/Student.java
@@ -5,11 +5,29 @@ import java.util.List;
 
 public class Student {
 
-    String name;
-    List<Course> courses = new ArrayList<>();
+    final String name;
+    final List<Course> courses = new ArrayList<>();
+
+    Student(String name) {
+        this.name = name;
+    }
 
     void addCourse(Course course) {
         this.courses.add(course);
         course.students.add(this);
+    }
+
+    Course couserPerName(String name) {
+        for (Course course : this.courses) {
+            if (course.name.equalsIgnoreCase(name)) {
+                return course;
+            }
+        }
+
+        return null;
+    }
+
+    public String toString() {
+        return name;
     }
 }
