3ac68a6aab781ede2fa6efe0ed2c9d92ed9c76c6
Wendson <wendsonsantanajps@gmail.com>
2022-01-15 12:12:58 -0300

    Exercise four

--- javaStudies/src/LogicExercises/Exercises.java
+++ javaStudies/src/LogicExercises/Exercises.java
@@ -16,11 +16,19 @@ public class Exercises {
         // System.out.println("Hello " + name + " nice to meet you");
 
         // Exercise 3
-        System.out.println("Enter name");
-        String name = entry.nextLine();
-        System.out.println("Salary?");
-        double salary = entry.nextDouble();
-        System.out.println("Employee " + name + " has a salary of R$" + salary + " in June");
+        // System.out.println("Enter name");
+        // String name = entry.nextLine();
+        // System.out.println("Salary?");
+        // double salary = entry.nextDouble();
+        // System.out.println("Employee " + name + " has a salary of R$" + salary + " in
+        // June");
+
+        // Exercise 4
+        System.out.println("Enter value");
+        int n1 = entry.nextInt();
+        System.out.println("Enter value");
+        int n2 = entry.nextInt();
+        System.out.println("The sum between " + n1 + " and " + n2 + " equals " + (n1 + n2));
 
         entry.close();
 
